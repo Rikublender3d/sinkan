a45d4570bcc45f56eca1191184231a8e66035704
Mblender3d <mikublender3d@gmail.com>
2025-04-10 14:33:00 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import { SiX, SiFacebook, SiInstagram } from '@icons-pack/react-simple-icons';
+import { ClipboardCopy } from 'lucide-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative">
+        {/* ヘッダーセクション */}
+        <header className="py-6">
+          <nav className="container mx-auto px-4">
+            <div className="flex items-center justify-between">
+              <Link
+                to="/"
+                className="righteous-regular text-[#1c1e56] text-3xl font-bold "
+              >
+                Beyond2020NextProject
+              </Link>
+              <div className="space-x-8">
+                <Link
+                  to="/"
+                  className="text-sm font-medium text-slate-400 transition-colors duration-200 hover:text-white"
+                >
+                  Home
+                </Link>
+                <a href='#about' className="text-sm font-medium text-slate-400 transition-colors duration-200 hover:text-white">
+                  AboutUs
+                </a>
+                <a href='#skills' className="text-sm font-medium text-slate-400 transition-colors duration-200 hover:text-white">
+                  Schedule
+                </a>
+                <Link
+                  to="#"
+                  className="text-sm font-medium text-slate-400 transition-colors duration-200 hover:text-white"
+                >
+                  参加申込はこちら
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <footer className="bg-gray-800 text-white py-4">
+           {/* シェアボタン */}
+           <div className="my-10 flex flex-col items-center text-center">
+                    <div className="flex items-center justify-center gap-4">
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    `https://x.com/w_beyond2020`,
+                                    '_blank'
+                                )
+                            }
+                            className="group inline-flex h-12 w-12 items-center justify-center rounded-xl border border-slate-800 bg-slate-900/50 text-slate-400 transition-all duration-300 hover:border-purple-500/50 hover:text-white"
+                        >
+                            <SiX className="h-5 w-5" />
+                        </button>
+
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    `https://www.facebook.com/beyond2020nextproject/`,
+                                    '_blank'
+                                )
+                            }
+                            className="group inline-flex h-12 w-12 items-center justify-center rounded-xl border border-slate-800 bg-slate-900/50 text-slate-400 transition-all duration-300 hover:border-purple-500/50 hover:text-white"
+                        >
+                            <SiFacebook className="h-5 w-5" />
+                        </button>
+
+                        <button
+                            onClick={() =>
+                                window.open(
+                                    `https://www.instagram.com/beyond2020nextproject/`,
+                                    '_blank'
+                                )
+                            }
+                            className="group inline-flex h-12 w-12 items-center justify-center rounded-xl border border-slate-800 bg-slate-900/50 text-slate-400 transition-all duration-300 hover:border-purple-500/50 hover:text-white"
+                        >
+                            <SiInstagram className="h-5 w-5" />
+                        </button>
+                    </div>
+                </div>
+          <div className="container mx-auto text-center">
+            <p>&copy; 2023 Beyond2020NextProject. All rights reserved.</p>
+          </div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,97 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import Image from '../assets/profile.jpg';
+import one from '../assets/one.jpg';
+import two from '../assets/two.jpg';
+import three from '../assets/three.jpg';
+AOS.init();
+export default function Home() {
+    return (
+        <div className="relative">
+            <div className="area w-full h-screen absolute bg-gradient-to-l from-[#8f94fb] to-[#4e54c8] -z-10">
+                <ul className="circles relative w-full h-full overflow-hidden">
+                    <li className="circle circle1"></li>
+                    <li className="circle circle2"></li>
+                    <li className="circle circle3"></li>
+                    <li className="circle circle4"></li>
+                    <li className="circle circle5"></li>
+                    <li className="circle circle6"></li>
+                    <li className="circle circle7"></li>
+                    <li className="circle circle8"></li>
+                    <li className="circle circle9"></li>
+                    <li className="circle circle10"></li>
+                </ul>
+            </div>
+            <div className="mx-auto w-full w-full text-center fade-in" data-aos="fade-up" data-aos-duration="1000">
+                <div className="h-screen w-full flex justify-center items-center">
+                    <h2 className="righteous-regular p-3 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 bg-clip-text text-6xl font-bold tracking-tight text-transparent">
+                        Beyond 2020 NEXT PROJECT
+                    </h2>
+                </div>
+                <h2 className="p-3 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 bg-clip-text text-6xl font-bold tracking-tight text-transparent" id="about">
+                    AboutUs
+                </h2>
+                <h3 className='mt-2 text-base text-gray-600'>Beyond 2020 Next Projectとは</h3>
+                <div className="max-w-5xl mx-auto mt-10 flex items-center justify-between gap-6 column-3" data-aos="fade-up" data-aos-duration="1000">
+                    <img src={Image} alt="Profile" className="rounded-full w-1/2 h-1/2" />
+                    <div className="text-left w-1/2">
+                        <p className="mt-2 text-2xl text-gray-600">
+                            Beyond 2020 NEXT PROJECTは、
+                            "アントレプレナーシップを養成する"ことをMISSIONに掲げた、早稲田大学発の学生団体です。アントレプレナーシップとは、起業家精神と直訳されますが、当団体では起業のみならず、全人類が持ち合わせるべきゼロイチ力と捉えています。高校生から大学院生の幅広い世代で構成されるメンバーは、これまで存在しなかった新しい価値を生み出す過程を経験します。
+                        </p>
+                    </div>
+                </div>
+                <h2 className="p-3 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 bg-clip-text text-6xl font-bold tracking-tight text-transparent mt-10" id="project">
+                    Project
+                </h2>
+                <div className=" flex w-[100%] flex-wrap	justify-evenly  mt-10 items-left">
+                    <div class="my-5 flex-col w-[45%] rounded overflow-hidden shadow-2xl" data-aos="flip-up" data-aos-duration="500">
+                        <img class="object-cover w-full mx-auto" src={one} alt="Sunset in the mountains"></img>
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">国連を支える世界こども未来会議</div>
+                            <p class="text-gray-700 text-base">
+                                国連を支える世界こども未来会議とは、一般財団法人ピースコミュニケーション財団が主催する世界の子どもたちが集まって平和で豊かな世界について考えるプログラムです。私たちは2030年頃に成人する子どもたちが集い、「平和な世界」「豊かな世界」について語り合うピースコミュニケーション の場を作ることを目的とし、コミュニケーションサポーターとして議論の進行を担います。
+                                2023年度は沖縄、浜松市、大阪、品川区、豊島区、北区で実施しました。そして、各地域で受賞したこどもたちと世界各国から集まるこどもたちが集う第4回国連を支える世界こども未来会議の開催を予定しています。
+                            </p>
+                        </div>
+                    </div>
+                    <div class=" my-5 flex-col w-[45%] rounded overflow-hidden shadow-lg" data-aos="flip-up" data-aos-duration="1000">
+                        <img class="object-cover w-full mx-auto" src={two} alt="Sunset in the mountains"></img>
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">アントレプレナーシップ教育</div>
+                            <p class="text-gray-700 text-base">
+                                アントレプレナーシップ教育プロジェクトは、私たちが普段大学や当団体などで学んだアントレプレナーシップを、イノベーションの創出を牽引する人材の育成を目指し、実践するプロジェクトです。現在は、小学校・中学校・高校に出張講義としてアントレプレナーシップワークショップを実施しています。
+                                2023年度は、神奈川県立保健福祉大学が主催する川崎市立川崎高校附属中学校におけるアントレプレナーシップワークショップにファシリテーターとして授業の進行を務めました。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="my-5 flex-col w-[45%] rounded overflow-hidden shadow-lg" data-aos="flip-up" data-aos-duration="1500">
+                        <img class="object-cover w-full mx-auto" src={three} alt="Sunset in the mountains"></img>
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">SDGs教育事業                            </div>
+                            <p class="text-gray-700 text-base">
+                                SDGs×アントレプレナーシップ教育カリキュラムプロジェクトでは、小学生にSDGsとアントレプレナーシップを伝え、”持続可能な開発”ネイティブを育成することを目的として活動しています。これまで180名以上の小学生を対象に、SDGsの視点から、身近なテーマをベースに新しいアイデアを創出することを通してアントレプレナーシップの重要性を学ぶワークショップを実施してきました。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="my-5 flex-col w-[45%] rounded overflow-hidden shadow-lg" data-aos="flip-up" data-aos-duration="2000">
+                        <img class="object-cover mx-auto" src={Image} alt="Sunset in the mountains"></img>
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">学生発企画 </div>
+                            <p class="text-gray-700 text-base">
+                                私たちは、アントレプレナーシップの向上と縦横のネットワークの展開を狙いとし、各学生が自身で企画したイベントの開催を行なっております。イベントは早稲田大学やSHIBUYA QWS、TOKYO創業ステーションなどで開催しており、内容は各自の専門を活かしたアカデミックな領域からビジネスやアートに関する領域まで多岐に渡ります。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="p-3 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 bg-clip-text text-6xl font-bold tracking-tight text-transparent mt-10" id="shedule">
+                    Schedule
+                </h2>
+                <div>
+                    新入生歓迎会の開催を予定しています。<br></br>
+                    画像埋め込み
+                </div>
+                </div>
+            </div>
+            )
+}

d570f7a05079b14f2859170fad8b4abd72dafbab
Mblender3d <mikublender3d@gmail.com>
2025-04-15 22:52:27 +0900

    add hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-        <Router basename="/">
+    <HashRouter basename="/">
     <App />
-        </Router>
+    </HashRouter>
 )
